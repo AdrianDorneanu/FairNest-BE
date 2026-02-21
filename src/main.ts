fab2fffabb4cae9e2e99e44330e7e5daea03692b
AdiDrn <adrian.dorneanu@zig.nl>
2026-02-21 14:16:44 +0100

    Added CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    credentials: true,
+  });
+
   app.setGlobalPrefix('api');
   app.enableVersioning({
     type: VersioningType.URI,
