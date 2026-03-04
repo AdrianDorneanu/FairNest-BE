ee41dc71b868e2f6b509fdd551148dd15b5a209f
AdiDrn <adrian.dorneanu@zig.nl>
2026-03-04 17:22:30 +0100

    Run prettier

--- src/couple/dto/invite-partner.dto.ts
+++ src/couple/dto/invite-partner.dto.ts
@@ -1,12 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class InvitePartnerDto {
   @ApiProperty({
     description: 'Email of the partner to invite',
     example: 'partner@example.com',
   })
-  @IsEmail()
-  @IsNotEmpty()
   partnerEmail: string;
 }
